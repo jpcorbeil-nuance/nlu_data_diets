3c01bb302a0149e7f8aefb3733db06db7ba24a6a
Jean-Philippe Corbeil <jean-philippe.corbeil@nuance.com>
2022-12-30 13:05:41 -0500

    Add options and clean GPU

--- hf_nlu/scripts/av_run.sh
+++ hf_nlu/scripts/av_run.sh
@@ -2,26 +2,24 @@
 
 export PYTHONPATH=$PYTHONPATH:$HF_NLU_DATA_DIET
 
-# nvidia-smi;
-# CUDA_VISIBLE_DEVICES=$(python -m hf_nlu.extract_gpu)
-# echo $CUDA_VISIBLE_DEVICES
-
 random_seed=$RANDOM;
 mkdir -p $RUNFOLDER/$DATASET
 mkdir -p $RUNFOLDER/$DATASET/$random_seed
 echo "New RUN - $random_seed";
 
-# debug with CUDA_LAUNCH_BLOCKING=1
-# CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 python -m hf_nlu.nlu-training \
 --output_path $RUNFOLDER/$DATASET/$random_seed \
 --dataset_path $RUNPATH/datasets/$DATASET/nlu_data \
+--static_score_file_path $RUNPATH/scores/$SCORE_PATH \
 --random_seed $random_seed \
 --frequency $FREQUENCY \
 --prune_epoch $PRUNE_EPOCH \
 --prune_size $PRUNE_SIZE \
 --prune_offset $PRUNE_OFFSET \
---prune_mode $PRUNE_MODE
+--prune_mode $PRUNE_MODE \
+--prune_ema_var_coef $VAR_COEF
+# --prune_ema_use_var
+
 
 # Remove heavy checkpoints
 rm -r $RUNFOLDER/$DATASET/$random_seed/checkpoint-*/
